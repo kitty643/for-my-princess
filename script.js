d034daf0eec65b8d5c2e96a9b85fce4f991203d4
kitty643 <suryayadav2453@gmail.com>
2026-07-20 21:02:22 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,210 @@
 
+// MUSIC CONTROL
+
+const music = document.getElementById("music");
+
+
+function startStory(){
+
+    if(music){
+
+        music.play().catch(()=>{
+
+            console.log("Music waiting for user interaction");
+
+        });
+
+    }
+
+
+    document.querySelector(".hero").style.display="none";
+
+
+    document.querySelector(".hidden-message").style.display="flex";
+
+
+    createHearts();
+
+}
+
+
+
+
+// CREATE FLOATING HEARTS
+
+function createHearts(){
+
+
+    setInterval(()=>{
+
+
+        const heart=document.createElement("div");
+
+
+        heart.innerHTML="❤️";
+
+
+        heart.style.position="fixed";
+
+        heart.style.left=Math.random()*100+"vw";
+
+        heart.style.bottom="-20px";
+
+        heart.style.fontSize=(Math.random()*25+15)+"px";
+
+
+        heart.style.animation="floatHeart 6s linear";
+
+
+        heart.style.zIndex="20";
+
+
+        document.body.appendChild(heart);
+
+
+
+        setTimeout(()=>{
+
+            heart.remove();
+
+        },6000);
+
+
+
+    },400);
+
+
+
+}
+
+
+
+
+
+// BUTTON CLICK HEART BURST
+
+
+document.addEventListener("click",function(e){
+
+
+    for(let i=0;i<8;i++){
+
+
+        let heart=document.createElement("span");
+
+
+        heart.innerHTML="💗";
+
+
+        heart.style.position="fixed";
+
+        heart.style.left=e.clientX+"px";
+
+        heart.style.top=e.clientY+"px";
+
+        heart.style.fontSize="20px";
+
+        heart.style.pointerEvents="none";
+
+
+        heart.style.animation="burst 1s forwards";
+
+
+        document.body.appendChild(heart);
+
+
+
+        setTimeout(()=>{
+
+            heart.remove();
+
+        },1000);
+
+
+    }
+
+
+});
+
+
+
+
+
+// ADD ANIMATION CSS FROM JS
+
+const style=document.createElement("style");
+
+
+style.innerHTML=`
+
+@keyframes floatHeart{
+
+0%{
+
+transform:translateY(0) rotate(0deg);
+
+opacity:1;
+
+}
+
+
+100%{
+
+transform:translateY(-120vh) rotate(360deg);
+
+opacity:0;
+
+}
+
+}
+
+
+
+@keyframes burst{
+
+
+0%{
+
+transform:translate(0,0) scale(1);
+
+opacity:1;
+
+}
+
+
+100%{
+
+transform:translate(
+${Math.random()*200-100}px,
+${Math.random()*200-100}px
+)
+scale(0);
+
+opacity:0;
+
+}
+
+}
+
+`;
+
+
+document.head.appendChild(style);
+
+
+
+
+
+// START HEART EFFECT AFTER LOAD
+
+window.onload=function(){
+
+
+    setTimeout(()=>{
+
+        createHearts();
+
+    },4000);
+
+
+};
